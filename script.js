2056c347797c168828ebb75700c2d38f6ec67d40
Luuuuna <vvaskaa@list.ru>
2023-05-29 23:11:04 +0500

    Урок 2.10. Практика. Пишем приложение. Часть 1

--- script.js
+++ script.js
@@ -12,15 +12,80 @@ let appData = {
     savings: false
 }
 
-/* задаем пользовалю вопросы по 2 раза */
-let questionExpenses = prompt('Введите обязательную статью расходов в этом месяце', '');
-let questionAmount = +prompt('Во сколько обойдется?', '');
-let questionExpenses2 = prompt('Введите обязательную статью расходов в этом месяце', '');
-let questionAmount2 = +prompt('Во сколько обойдется?', '');
 
-/* Создаем дополнительные переменные в объекте "expenses" и принимаем данные от пользователя в них*/
-appData.expenses.amount = `${questionExpenses} : ${questionAmount}`;
-appData.expenses.amount2 = `${questionExpenses2} : ${questionAmount2}`;
+
+
+
+/* задаем пользовалю вопросы по 2 раза */
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдется?', '');
+
+    if ( (typeof(a) ) === 'string' && (typeof(a)) != null && (typeof(b)) != null  && a != ' ' && b != " "
+    && a.length < 50)  {
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        console.log ("bad result");
+        i--;
+    }
+   
+};
+
+
+// Используем цикл WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+
+
+// Используем цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
+
+appData.moneyPerDay = appData.moneyData / 30;
 
 // Выводим на экран пользователя бюджет за 1 день из расчета, что у нас месяц = 30 дней
-alert(`Ваш бюджет на 1 день =  ${appData.moneyData / 30}`);
+alert('Ваш бюджет на 1 день '  + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+}  else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
+} else {
+    console.log('Произошла ошибка');
+}
